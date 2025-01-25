5c9795f4b176b5c3bcc71919103b1af620287958
Naib2024 <naibn149@gmail.com>
2025-01-26 00:33:14 +0400

    create for data

---
+++ services/GetAllUsers.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const url = "http://localhost:4000/user/" 
+
+
+const getAllUsers = async () => {
+  const {data} = await axios.get(url)
+  return data
+  
+    
+};  
+
+export default getAllUsers;
