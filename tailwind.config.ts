85ea4c78fef530f8eaeb5f98d504588e2f16e31d
Naib2024 <naibn149@gmail.com>
2025-02-07 13:29:59 +0400

    added bg img

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage:{
+        bannerImg:"url('../public/opening.svg')",
+      },
     },
   },
   plugins: [],
