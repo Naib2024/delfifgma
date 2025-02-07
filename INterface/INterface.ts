12bb3b9eae39667b0e6990cd955d2cf07f421aad
Naib2024 <naibn149@gmail.com>
2025-02-07 13:31:21 +0400

    iptade

--- INterface/INterface.ts
+++ INterface/INterface.ts
@@ -1,22 +1,23 @@
-export interface IUsers{
-    id:number;
-    discription:string;
-    name:string;
-    price:number|string
-    img:string
-    category:string
-    isOutline:false
-    onClick:string
-  }
-
-export interface IButton{
-  text:string;
-
+export interface IUsers {
+  id: number;
+  discription: string;
+  name: string;
+  price: number | string
+  img: string
+  category: string
+  isOutline: false
+  onClick: string
 }
 
-export interface IChef{
+export interface IButton {
+  text: string;
+  onClick():void
+  isOutline?: boolean;  
+}
+
+export interface IChef {
   id: number;
   name: string;
   position: string;
-  img:string
+  img: string
 }
